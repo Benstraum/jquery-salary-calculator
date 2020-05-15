0c7c863fb5dd1bec6140c1dba8cea8f8018242f3
baaiinn <benstraum@gmail.com>
2020-05-15 18:07:34 -0500

    need to learn better way to target table cell

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function readyNow() {
 
     $('#submitBtn').on('click', submitButton)
 
-
 };
 
 function submitButton() {
@@ -24,26 +23,60 @@ function submitButton() {
 
     };
     employeeArr.push(personObject);
-        $('#firstName').val('');
-        $('#lastName').val('');
-        $('#iD').val('');
-        $('#title').val('');
-        $('#annualSalary').val('');
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#iD').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
 
     appendToDom(employeeArr);
 }
 console.log(employeeArr);
 
-function appendToDom(array){
+function appendToDom(array) {
     $('#tableBody').empty();
-    for(el of array){
+    for (el of array) {
+        if(parseInt(el.annualSalary)>=20001){
         $('#tableBody').append(`
             <tr>
             <th>${el.firstName}</th>
-            <th>${el.lastName}</th>
+            <th >${el.lastName}</th>
+            <th>${el.iD}</th>
+            <th>${el.title}</th>
+            <th style = "background-color:red;">${el.annualSalary}</th>
+            <th><button id="deleteButton">Delete</button></th>
+            </th>`)
+        } else {
+            $('#tableBody').append(`
+            <tr>
+            <th>${el.firstName}</th>
+            <th >${el.lastName}</th>
             <th>${el.iD}</th>
             <th>${el.title}</th>
             <th>${el.annualSalary}</th>
+            <th><button id="deleteButton">Delete</button></th>
             </th>`)
+        }
+
+
     }
+
+    appendTotal(employeeArr)
 };
+
+
+let result = 0;
+
+
+function appendTotal(array) {
+    $('#totalCost').empty()
+
+    result = 0;
+    for (el of array) {
+
+        result += parseInt(el.annualSalary)
+
+    }
+
+    $('#totalCost').append(result)
+}
